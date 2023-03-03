e3de3c8bf93b014a41b36000d846c99ea72d3d4d
Ernest Korir <ernestbore3@gmail.com>
2023-03-04 00:16:58 +0300

    Modified files

--- src/index.js
+++ src/index.js
@@ -16,10 +16,5 @@ root.render(
         <Route path="/categories" element={<Categories />} />
       </Routes>
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
